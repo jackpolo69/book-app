00904203d26910949569e65cffe3dc540a95357e
Jack Polo <jackpolo@Jacks-MacBook-Pro.local>
2019-08-12 21:40:33 -0400

    post build

---
+++ client/build/precache-manifest.fc6499df2463a17451ea85b4290eab2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7f08ef539c95a15f6dd",
+    "url": "/static/css/main.c24ee4a8.chunk.css"
+  },
+  {
+    "revision": "e7f08ef539c95a15f6dd",
+    "url": "/static/js/main.6e0f800c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "283046fabc7e680ef29d",
+    "url": "/static/js/2.df965624.chunk.js"
+  },
+  {
+    "revision": "9599a3763093d524bb30bbf2a8f954ff",
+    "url": "/index.html"
+  }
+];
